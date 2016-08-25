49d2bbf279de85a7d91e285a6cc92b64ee8e8558
Emilie Pitre <Emilie Pitre>
2016-08-25 11:29:02 -0500

    practice

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1920, 1080);
+  fill(0)
+  ellipse(100, 100, 50);
+  fill(150)
+  strokeWeight(0)
+  ellipse(50, 100, 5,20);
+  strokeWeight(5)
+  ellipse(400, 200, 60,75);
+  ellipse(300, 100, 50, 30);
+
+}
