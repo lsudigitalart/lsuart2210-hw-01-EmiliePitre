0126730b0c54eac7131596ea440b1c40dc0b72cf
Emilie Pitre <Emilie Pitre>
2016-08-29 22:17:01 -0500

    submit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(1000, 700);
+
   fill(0)
-  ellipse(100, 100, 50);
-  fill(150)
+  ellipse(50, 50, 1000);
+
+  fill(255)
   strokeWeight(0)
-  ellipse(50, 100, 5,20);
-  strokeWeight(8)
-  ellipse(400, 200, 60,75);
-  ellipse(300, 100, 50, 30);
+  ellipse(50, 50, 800);
+
+  fill(0)
+  ellipse(50, 50, 780);
+
+  fill(255)
+  strokeWeight(0)
+  ellipse(30, 500, 700);
+
+  fill(0)
+  ellipse(30, 500, 650);
+
+  fill(255)
+  strokeWeight(0)
+  ellipse(25, 150, 20);
+  ellipse(300, 100, 50);
+  ellipse(100, 100, 90);
+  ellipse(300, 500, 15);
+  ellipse(450, 350, 90);
+
+  fill(0)
+  ellipse(800, 100, 50);
+  ellipse(650, 600, 90);
+  ellipse(1000, 300, 500);
+  ellipse(450, 350, 80);
+  ellipse(25, 150, 16);
+
+  fill(255)
+  ellipse(800, 100, 40);
+  ellipse(630, 610, 30);
+
+
 
 }
